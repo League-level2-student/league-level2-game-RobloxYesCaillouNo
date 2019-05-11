4b50ce4e70628deea354aada8872d45ca3ce0cf0
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2019-05-11 15:36:06 -0700

    Done did it
    :smile:

---
+++ Pickaxe.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+
+public class Pickaxe extends Objects {
+
+	int miningInProgress;
+
+	public Pickaxe(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+		miningInProgress = 55;
+
+	}
+
+	public void update() {
+		super.update();
+
+	}
+
+	public void draw(Graphics g) {
+		g.drawImage(GamePanel.pickImg, x, y, width, height, null);
+
+	}
+
+}
